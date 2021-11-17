7cac093f5add590b7fc2d140737e0d342d0c273d
Shane Rogers <swrogers@gmail.com>
2021-11-17 16:59:03 -0600

    theme via pelican-theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,5 +53,6 @@ STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
                        }
 
+THEME = 'Peli-Kiera'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
