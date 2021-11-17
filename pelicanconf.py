b58c556c39cba32357726abc01e59617c27567b1
Shane Rogers <swrogers@gmail.com>
2021-11-17 16:49:45 -0600

    remove theme config

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,6 +53,5 @@ STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
                        }
 
-THEME = 'themes/Peli-Kiera'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
